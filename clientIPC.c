8ab61a2e1449d7652d053a4c2e46e25046971a46
Isaac Yen <isaacyen7@gmail.com>
2022-04-25 14:39:15 -0700

    clientIPC->serverIPC 1-way comm. implemented tested correct

---
+++ clientIPC.c
@@ -0,0 +1,43 @@
+// Client side C/C++ program to demonstrate Socket
+// programming
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+int main(int argc, char const* argv[])
+{
+	int sock = 0, valread; //initialize sock and valread
+	struct sockaddr_in serv_addr;
+	char* hello = "Hello from client msg changed"; // message to be sent
+	char buffer[1024] = { 0 };
+	//if sock < 0 socket creation error
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	// Convert IPv4 and IPv6 addresses from text to binary
+	// form
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<= 0) {
+		printf(
+			"\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))< 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	//sends the message to the server
+	send(sock, hello, strlen(hello), 0);
+	printf("Hello message sent\n");
+	valread = read(sock, buffer, 1024);
+	printf("%s\n", buffer);
+	return 0;
+}
---
+++ serverIPC.js
@@ -0,0 +1,10 @@
+// server
+require('net').createServer(function (socket) {
+    console.log("connected");
+
+    socket.on('data', function (data) {
+        console.log(data.toString());
+    });
+})
+
+.listen(8080);
